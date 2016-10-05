4df29762cfb3a9c39e7293f2be0d8d5fa6f0fc22
Justin Firth <jmfirth@gmail.com>
2016-10-04 23:15:21 -0400

    Add project files.

---
+++ ServerRenderedReactNetMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ServerRenderedReactNetMvc.Startup))]
+namespace ServerRenderedReactNetMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
